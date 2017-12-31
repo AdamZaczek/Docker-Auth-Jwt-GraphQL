3c3de6bbe7bbb008a04af24374788437aecbfd64
adamzaczek <adamzaczekul@gmail.com>
2017-12-31 02:07:45 +0100

    Add Some Templating For Easy Prototyping

--- src/app.js
+++ src/app.js
@@ -49,6 +49,9 @@ const app = express();
 
 app.set('trust proxy', 'loopback');
 
+// this is only for prototyping phase
+app.set('view engine', 'ejs');
+
 app.use(
   cors({
     origin(origin, cb) {
@@ -100,21 +103,24 @@ if (process.env.NODE_ENV !== 'production') {
 
   // A route for testing authentication/authorization
   app.get('/', (req, res) => {
-    if (req.user) {
-      res.send(
-        `<p>${req.t('Welcome, {{user}}!', {
-          user: req.user.displayName,
-        })} (<a href="javascript:fetch('/login/clear', { method: 'POST', credentials: 'include' }).then(() => window.location = '/')">${req.t(
-          'log out',
-        )}</a>)</p>`,
-      );
-    } else {
-      res.send(
-        `<p>${req.t('Welcome, guest!')} (<a href="/login/facebook">${req.t(
-          'sign in',
-        )}</a>)</p>`,
-      );
-    }
+    res.render('pages/index');
+
+    // this can still be somehow useful as a reference, leaving it for now
+    // if (req.user) {
+    //   res.send(
+    //     `<p>${req.t('Welcome, {{user}}!', {
+    //       user: req.user.displayName,
+    //     })} (<a href="javascript:fetch('/login/clear', { method: 'POST', credentials: 'include' }).then(() => window.location = '/')">${req.t(
+    //       'log out',
+    //     )}</a>)</p>`,
+    //   );
+    // } else {
+    //   res.send(
+    //     `<p>${req.t('Welcome, guest!')} (<a href="/login/facebook">${req.t(
+    //       'sign in',
+    //     )}</a>)</p>`,
+    //   );
+    // }
   });
 }
 
