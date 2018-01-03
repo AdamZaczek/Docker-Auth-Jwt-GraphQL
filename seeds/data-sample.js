d55507fe6b9322e5d38bb81459eca80a50debb17
adamzaczek <adamzaczekul@gmail.com>
2018-01-03 07:18:29 +0100

    Re-Add Some Code For A Moment

--- seeds/data-sample.js
+++ seeds/data-sample.js
@@ -43,4 +43,258 @@ module.exports.seed = async db => {
         .then(row => Object.assign(user, row)),
     ),
   );
+
+  // not removing this for now, can still be useful as a reference
+
+  // Create 500 stories
+  //   const stories = Array.from({
+  //     length: 500,
+  //   }).map(() =>
+  //     Object.assign(
+  //       {
+  //         author_id:
+  //           users[
+  //             faker.random.number({
+  //               min: 0,
+  //               max: users.length - 1,
+  //             })
+  //           ].id,
+  //         title: faker.lorem
+  //           .sentence(
+  //             faker.random.number({
+  //               min: 4,
+  //               max: 7,
+  //             }),
+  //           )
+  //           .slice(0, -1)
+  //           .substr(0, 80),
+  //       },
+  //       Math.random() > 0.3
+  //         ? {
+  //             text: faker.lorem.text(),
+  //           }
+  //         : {
+  //             url: faker.internet.url(),
+  //           },
+  //       (date => ({
+  //         created_at: date,
+  //         updated_at: date,
+  //       }))(faker.date.past()),
+  //     ),
+  //   );
+
+  //   await Promise.all(
+  //     stories.map(story =>
+  //       db
+  //         .table('stories')
+  //         .insert(story)
+  //         .returning('id')
+  //         .then(rows =>
+  //           db
+  //             .table('stories')
+  //             .where('id', '=', rows[0])
+  //             .first(),
+  //         )
+  //         .then(row => Object.assign(story, row)),
+  //     ),
+  //   );
+
+  //   // Create some user comments
+  //   const comments = Array.from({
+  //     length: 2000,
+  //   }).map(() =>
+  //     Object.assign(
+  //       {
+  //         story_id:
+  //           stories[
+  //             faker.random.number({
+  //               min: 0,
+  //               max: stories.length - 1,
+  //             })
+  //           ].id,
+  //         author_id:
+  //           users[
+  //             faker.random.number({
+  //               min: 0,
+  //               max: users.length - 1,
+  //             })
+  //           ].id,
+  //         text: faker.lorem.sentences(
+  //           faker.random.number({
+  //             min: 1,
+  //             max: 10,
+  //           }),
+  //         ),
+  //       },
+  //       (date => ({
+  //         created_at: date,
+  //         updated_at: date,
+  //       }))(faker.date.past()),
+  //     ),
+  //   );
+
+  //   await Promise.all(
+  //     comments.map(comment =>
+  //       db
+  //         .table('comments')
+  //         .insert(comment)
+  //         .returning('id')
+  //         .then(rows =>
+  //           db
+  //             .table('comments')
+  //             .where('id', '=', rows[0])
+  //             .first(),
+  //         )
+  //         .then(row => Object.assign(comment, row)),
+  //     ),
+  //   );
+  // };
+
+  // /* eslint-disable no-restricted-syntax, no-await-in-loop */
+  // import bcrypt from 'bcrypt';
+
+  // const faker = require('faker');
+
+  // module.exports.seed = async db => {
+  //   const salt = bcrypt.genSaltSync();
+  //   const hash = bcrypt.hashSync('noskateboarding', salt);
+  //   const users = [
+  //     {
+  //       username: 'Rodney Mullen',
+  //       password: hash,
+  //     },
+  //     {
+  //       username: 'Bob Burnquist',
+  //       password: hash,
+  //     },
+  //   ];
+
+  //   await Promise.all(
+  //     users.map(user =>
+  //       db
+  //         .table('users')
+  //         .insert(users)
+  //         .returning('id')
+  //         .then(rows =>
+  //           db
+  //             .table('users')
+  //             .where('id', '=', rows[0])
+  //             .first()
+  //             .then(u =>
+  //               db
+  //                 .table('emails')
+  //                 .insert({
+  //                   user_id: u.id,
+  //                   email: faker.internet.email().toLowerCase(),
+  //                 })
+  //                 .then(() => u),
+  //             ),
+  //         )
+  //         .then(row => Object.assign(user, row)),
+  //     ),
+  //   );
+
+  //   // Create 500 stories
+  //   const stories = Array.from({
+  //     length: 500,
+  //   }).map(() =>
+  //     Object.assign(
+  //       {
+  //         author_id:
+  //           users[
+  //             faker.random.number({
+  //               min: 0,
+  //               max: users.length - 1,
+  //             })
+  //           ].id,
+  //         title: faker.lorem
+  //           .sentence(
+  //             faker.random.number({
+  //               min: 4,
+  //               max: 7,
+  //             }),
+  //           )
+  //           .slice(0, -1)
+  //           .substr(0, 80),
+  //       },
+  //       Math.random() > 0.3
+  //         ? {
+  //             text: faker.lorem.text(),
+  //           }
+  //         : {
+  //             url: faker.internet.url(),
+  //           },
+  //       (date => ({
+  //         created_at: date,
+  //         updated_at: date,
+  //       }))(faker.date.past()),
+  //     ),
+  //   );
+
+  //   await Promise.all(
+  //     stories.map(story =>
+  //       db
+  //         .table('stories')
+  //         .insert(story)
+  //         .returning('id')
+  //         .then(rows =>
+  //           db
+  //             .table('stories')
+  //             .where('id', '=', rows[0])
+  //             .first(),
+  //         )
+  //         .then(row => Object.assign(story, row)),
+  //     ),
+  //   );
+
+  //   // Create some user comments
+  //   const comments = Array.from({
+  //     length: 2000,
+  //   }).map(() =>
+  //     Object.assign(
+  //       {
+  //         story_id:
+  //           stories[
+  //             faker.random.number({
+  //               min: 0,
+  //               max: stories.length - 1,
+  //             })
+  //           ].id,
+  //         author_id:
+  //           users[
+  //             faker.random.number({
+  //               min: 0,
+  //               max: users.length - 1,
+  //             })
+  //           ].id,
+  //         text: faker.lorem.sentences(
+  //           faker.random.number({
+  //             min: 1,
+  //             max: 10,
+  //           }),
+  //         ),
+  //       },
+  //       (date => ({
+  //         created_at: date,
+  //         updated_at: date,
+  //       }))(faker.date.past()),
+  //     ),
+  //   );
+
+  //   await Promise.all(
+  //     comments.map(comment =>
+  //       db
+  //         .table('comments')
+  //         .insert(comment)
+  //         .returning('id')
+  //         .then(rows =>
+  //           db
+  //             .table('comments')
+  //             .where('id', '=', rows[0])
+  //             .first(),
+  //         )
+  //         .then(row => Object.assign(comment, row)),
+  //     ),
+  //   );
+  // };
 };
--- src/passport.js
+++ src/passport.js
@@ -3,6 +3,7 @@
 /* to do - make new folder for passport strategies */
 
 import passport from 'passport';
+import { Strategy as FacebookStrategy } from 'passport-facebook';
 import db from './db';
 import { comparePass } from './helpers/auth';
 
@@ -17,6 +18,105 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((user, done) => {
   done(null, user);
 });
+
+// Creates or updates the external login credentials
+// and returns the currently authenticated user.
+async function login(req, provider, profile, tokens) {
+  let user;
+
+  if (req.user) {
+    user = await db
+      .table('users')
+      .where('id', '=', req.user.id)
+      .first();
+  }
+
+  if (!user) {
+    user = await db
+      .table('logins')
+      .innerJoin('users', 'users.id', 'logins.user_id')
+      .where({
+        'logins.provider': provider,
+        'logins.id': profile.id,
+      })
+      .first('users.*');
+    if (
+      !user &&
+      profile.emails &&
+      profile.emails.length &&
+      profile.emails[0].verified === true
+    ) {
+      user = await db
+        .table('users')
+        .innerJoin('emails', 'emails.user_id', 'users.id')
+        .where({
+          'emails.email': profile.emails[0].value,
+          'emails.verified': true,
+        })
+        .first('users.*');
+    }
+  }
+
+  if (!user) {
+    // eslint-disable-next-line prefer-destructuring
+    user = (await db
+      .table('users')
+      .insert({
+        username: profile.displayName,
+        image_url:
+          profile.photos && profile.photos.length
+            ? profile.photos[0].value
+            : null,
+      })
+      .returning('*'))[0];
+
+    if (profile.emails && profile.emails.length) {
+      await db.table('emails').insert(
+        profile.emails.map(x => ({
+          user_id: user.id,
+          email: x.value,
+          verified: x.verified || false,
+        })),
+      );
+    }
+  }
+
+  const loginKeys = {
+    user_id: user.id,
+    provider,
+    id: profile.id,
+  };
+  const { count } = await db
+    .table('logins')
+    .where(loginKeys)
+    .count('id')
+    .first();
+
+  if (count === '0') {
+    await db.table('logins').insert({
+      ...loginKeys,
+      username: profile.username,
+      tokens: JSON.stringify(tokens),
+      profile: JSON.stringify(profile._json),
+    });
+  } else {
+    await db
+      .table('logins')
+      .where(loginKeys)
+      .update({
+        username: profile.username,
+        tokens: JSON.stringify(tokens),
+        profile: JSON.stringify(profile._json),
+        updated_at: db.raw('CURRENT_TIMESTAMP'),
+      });
+  }
+
+  return {
+    id: user.id,
+    displayName: user.display_name,
+    imageUrl: user.image_url,
+  };
+}
 
 const LocalStrategy = require('passport-local').Strategy;
 
@@ -38,6 +138,49 @@ passport.use(
       })
       .catch(err => done(err));
   }),
+);
+
+// https://github.com/jaredhanson/passport-facebook
+// https://developers.facebook.com/docs/facebook-login/permissions/
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      profileFields: [
+        'id',
+        'cover',
+        'name',
+        'age_range',
+        'link',
+        'gender',
+        'locale',
+        'picture',
+        'timezone',
+        'updated_time',
+        'verified',
+        'email',
+      ],
+      callbackURL: '/login/facebook/return',
+      passReqToCallback: true,
+    },
+    async (req, accessToken, refreshToken, profile, done) => {
+      try {
+        if (profile.emails.length)
+          profile.emails[0].verified = !!profile._json.verified;
+        profile.displayName =
+          profile.displayName ||
+          `${profile.name.givenName} ${profile.name.familyName}`;
+        const user = await login(req, 'facebook', profile, {
+          accessToken,
+          refreshToken,
+        });
+        done(null, user);
+      } catch (err) {
+        done(err);
+      }
+    },
+  ),
 );
 
 export default passport;
