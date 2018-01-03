5dba3860077c3ed8781bf8261e2adf7d4fabc3bb
adamzaczek <adamzaczekul@gmail.com>
2018-01-03 07:06:18 +0100

    Add Totaly Untested Code Because This Is The Best Practice Nowadays

--- src/routes/account.js
+++ src/routes/account.js
@@ -8,53 +8,6 @@ import { createUser, loginRequired } from '../helpers/auth';
 
 const router = new Router();
 
-// commenting for now, might prove usefull
-
-// '/about' => ''
-// http://localhost:3000/some/page => http://localhost:3000
-// function getOrigin(url: string) {
-//   if (!url || url.startsWith('/')) return '';
-//   return (x => `${String(x.protocol)}//${String(x.host)}`)(URL.parse(url));
-// }
-
-// '/about' => `true` (all relative URL paths are allowed)
-// 'http://localhost:3000/about' => `true` (but only if its origin is whitelisted)
-// function isValidReturnURL(url: string) {
-//   if (url.startsWith('/')) return true;
-//   const whitelist = process.env.CORS_ORIGIN
-//     ? process.env.CORS_ORIGIN.split(',')
-//     : [];
-//   return (
-//     validator.isURL(url, {
-//       require_tld: false,
-//       require_protocol: true,
-//       protocols: ['http', 'https'],
-//     }) && whitelist.includes(getOrigin(url))
-//   );
-// }
-
-// Generates a URL for redirecting a user to upon successfull authentication.
-// It is intended to support cross-domain authentication in development mode.
-// For example, a user goes to http://localhost:3000/login (frontend) to sign in,
-// then he's being redirected to http://localhost:8080/login/facebook (backend),
-// Passport.js redirects the user to Facebook, which redirects the user back to
-// http://localhost:8080/login/facebook/return and finally, user is being redirected
-// to http://localhost:3000/?sessionID=xxx where front-end middleware can save that
-// session ID into cookie (res.cookie.sid = req.query.sessionID).
-
-// function getSuccessRedirect(req) {
-//   const url = req.query.return || req.body.return || '/';
-//   if (!isValidReturnURL(url)) return '/';
-//   if (!getOrigin(url)) return url;
-//   return `${url}${url.includes('?') ? '&' : '?'}sessionID=${req.cookies.sid}${
-//     req.session.cookie.originalMaxAge
-//       ? `&maxAge=${req.session.cookie.originalMaxAge}`
-//       : ''
-//   }`;
-// }
-
-/* in progress */
-
 function handleResponse(res, code, statusMsg) {
   res.status(code).json({
     status: statusMsg,
@@ -67,6 +20,7 @@ router.post('/auth/register', (req, res, next) =>
       passport.authenticate('local', (err, user) => {
         if (user) {
           handleResponse(res, 200, 'success');
+          res.redirect('/');
         }
       })(req, res, next);
     })
@@ -87,6 +41,7 @@ router.post('/auth/login', (req, res, next) => {
           handleResponse(res, 500, 'error');
         }
         handleResponse(res, 200, 'success');
+        res.redirect('/');
       });
     }
   })(req, res, next);
@@ -95,6 +50,7 @@ router.post('/auth/login', (req, res, next) => {
 router.get('/auth/logout', loginRequired, (req, res) => {
   req.session.destroy();
   handleResponse(res, 200, 'success');
+  res.redirect('/');
 });
 
 // Allows to fetch the last login error(s) (which is usefull for single-page apps)
