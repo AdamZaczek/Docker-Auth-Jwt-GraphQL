c01e8ca7cf0800c814607874dd40d36f1ceb1b2d
adamzaczek <adamzaczekul@gmail.com>
2018-01-20 23:31:07 +0100

    Remove Commented Code

--- src/__tests__/integration/register.js
+++ src/__tests__/integration/register.js
@@ -19,20 +19,6 @@ describe('routes : auth', () => {
 
   afterEach(() => db.migrate.rollback());
 
-  // beforeEach(() =>
-  //   db.migrate
-  //     .rollback()
-  //     .then(() => db.migrate.latest())
-  //     .then(() => db.seed.run()),
-  // );
-
-  // this is causing knex_migration_lock table error because
-  // there are no sessions anymore so no logout function
-  // afterEach(() =>
-  //   passportStub.logout();
-  //   db.migrate.rollback(),
-  // );
-
   describe('POST /auth/register', () => {
     it('should register a new user', done => {
       chai
@@ -52,23 +38,6 @@ describe('routes : auth', () => {
         });
     });
   });
-
-  // describe('POST /auth/register', () => {
-  //   it('should register a new user', done => {
-  //     chai
-  //       .request(server)
-  //       .get('/')
-  //       .end((err, res) => {
-  //         // console.log(res, err);
-  //         // should.not.exist(err);
-  //         // res.redirects.length.should.eql(0);
-  //         // res.status.should.eql(200);
-  //         // res.type.should.eql('application/json');
-  //         // res.body.status.should.eql('success');
-  //         done();
-  //       });
-  //   });
-  // });
 
   describe('POST /auth/login', () => {
     it('should login a user', done => {
